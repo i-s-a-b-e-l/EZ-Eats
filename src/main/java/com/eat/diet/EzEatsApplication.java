d200f3f470f2fed48a1367753359e2d5ddeab799
i-s-a-b-e-l <109228682+i-s-a-b-e-l@users.noreply.github.com>
2023-05-11 11:24:24 -0400

    Initial commit

---
+++ src/main/java/com/eat/diet/EzEatsApplication.java
@@ -0,0 +1,13 @@
+package com.eat.diet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzEatsApplication.class, args);
+	}
+
+}

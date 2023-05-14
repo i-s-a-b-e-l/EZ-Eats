2a594e6eed6bfe68951f642688ac4e46ac068d8e
mahima <mahimasri02@gmail.com>
2023-05-14 13:35:18 -0400

    attributes for food class

--- src/main/java/com/eat/diet/repo/model/Food.java
+++ src/main/java/com/eat/diet/repo/model/Food.java
@@ -14,5 +14,42 @@ public class Food {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
+    private int calories;
+    private int serving;
+    private String type; //Breakfast, Lunch, or Dinner
+
+    public Food(){
+        name = " ";
+        calories = 0;
+        serving = 0;
+    }
+
+    public void setCalories(int calories){
+        this.calories = calories;
+    }
+
+    public int getCalories(){
+        return calories;
+    }
+
+    public void setServing(int serving){
+        this.serving = serving;
+    }
+
+    public int getServing(){
+        return serving;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+
+
+
 }
 

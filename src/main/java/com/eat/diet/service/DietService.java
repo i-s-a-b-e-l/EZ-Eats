64c21ebc87cc0eac7db4ca8f21368793d1bd4d84
mahima <mahimasri02@gmail.com>
2023-05-14 14:18:42 -0400

    started calc method, did not finish yet

--- src/main/java/com/eat/diet/service/DietService.java
+++ src/main/java/com/eat/diet/service/DietService.java
@@ -19,4 +19,59 @@ public class DietService {
         return foodRepository.save(food);
 
     }
+
+    public int calc(String goal, int weight, String gender, String activityLevel, int bodyFat){
+        if(goal.equals("Maintain Weight")){
+            double kg = weight/2.2;
+            if(gender.equals("Female")){
+                double BMR = kg * 0.9 * 24;
+                if(bodyFat <= 18){
+                    BMR = BMR * 1.0;
+                }
+                else if(bodyFat <= 28){
+                    BMR = BMR * 0.95;
+                }
+                else if(bodyFat <= 38){
+                    BMR = BMR * 0.90;
+                }
+                else{
+                    BMR = BMR * 0.85;
+                }
+                int calAmount = 0;
+                if(activityLevel.equals("Very Low")){
+                    calAmount = (int)(BMR * 1.3);
+                }
+                if(activityLevel.equals("Low")){
+                    calAmount = (int)(BMR * 1.55);
+                }
+                if(activityLevel.equals("Moderate")){
+                    calAmount = (int)(BMR * 1.65);
+                }
+                if(activityLevel.equals("High")){
+                    calAmount = (int)(BMR * 1.8);
+                }
+                if(activityLevel.equals("Very High")){
+                    calAmount = (int)(BMR * 2.0);
+                }
+                return calAmount;
+
+            }
+            if(gender.equals("Male")){
+                double BMR = kg * 24;
+
+
+            }
+
+        }
+
+        if(goal.equals("Lose Weight")){
+
+        }
+
+        if(goal.equals("Gain Weight")){
+
+        }
+    }
+
+
 }
